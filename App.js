4cebf928d02ae0a76df017eb258e7be9f6ec7f65
PrachiPrajapati <prachiprajapati1911@gmail.com>
2023-10-10 14:21:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("div", {id:"parent"} , [React.createElement("div", {id:"child"} ,[React.createElement("h1",{},"Hey i'm h1 tag!" ),React.createElement("h2",{},"Hey i'm h2 tag!" )]),React.createElement("div", {id:"child2"} ,[React.createElement("h1",{},"Hey i'm h1 tag!" ),React.createElement("h2",{},"Hey i'm h2 tag!" )])]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
