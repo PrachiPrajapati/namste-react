a8e16093a9afcf3969011e1e2388a2c78dd5d582
PrachiPrajapati <prachiprajapati1911@gmail.com>
2023-10-12 15:33:51 +0530

    Episode 2 assignment

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 const heading = React.createElement("div", {id:"parent"} , [React.createElement("div", {id:"child"} ,[React.createElement("h1",{},"Hey i'm h1 tag!" ),React.createElement("h2",{},"Hey i'm h2 tag!" )]),React.createElement("div", {id:"child2"} ,[React.createElement("h1",{},"Hey i'm h1 tag!" ),React.createElement("h2",{},"Hey i'm h2 tag!" )])]);
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById('root'));
